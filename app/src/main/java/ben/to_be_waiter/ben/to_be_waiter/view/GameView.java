2bd1a83e5f97509bfd4e1299400419114f88b947
Bilal Ben Abdelkader <bilal.benabdelkader@realdolmen.com>
2018-03-01 21:05:46 +0100

    Refactor code view

--- app/src/main/java/ben/to_be_waiter/ben/to_be_waiter/view/GameView.java
+++ app/src/main/java/ben/to_be_waiter/ben/to_be_waiter/view/GameView.java
@@ -31,52 +31,36 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     // déclaration de l'objet définissant la boucle principale de déplacement et de rendu
     private GameLoopThread gameLoopThread;
 
-    private int h,w;
     private GameModel gameModel;
-    private Draw player;
-    public float hh;
-    private float xCor, yCor;
-    public float getHh() {
-        return hh;
-    }
+    private Draw playerDraw;
     private boolean ok=false;
-    private float xCorMove;
+    private int height;
+    private int width;
     private int xDelta;
-    private int yDelta;
+    private  int yPlayerDraw;
 
-    public void setHh(float hh) {
-        this.hh = hh;
-    }
 
     // création de la surface de dessin
     public GameView(Context context) {
         super(context);
         getHolder().addCallback(this);
         gameLoopThread = new GameLoopThread(this);
-
-        // création d'un objet "balle", dont on définira la largeur/hauteurF
-        // création d'un objet "balle", dont on définira la largeur/hauteurF
-        // selon la largeur ou la hauteur de l'écran
-
         WindowManager w = (WindowManager) this.getContext().getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics d =new DisplayMetrics();
         w.getDefaultDisplay().getMetrics(d);
-        h=d.heightPixels;
-        this.w = d.widthPixels;
-
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        int width = size.x;
-        int height = size.y;
+        width = size.x;
+        height = size.y;
+
         Resources resources = context.getResources();
         int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
-         float nbb=0;
-         float nbbb=0;
+        float nbb=0;
+        float nbbb=0;
         if (resourceId > 0) {
             nbb = resources.getDimensionPixelSize(resourceId);
-
         }
 
         int statusBarHeight = 0;
@@ -85,43 +69,22 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
             nbbb = getResources().getDimensionPixelSize(resourceId);
 
         }
-        int c = 0;
-        resourceId = getResources().getIdentifier("title_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            nbbb = getResources().getDimensionPixelSize(resourceId);
-
-        }
         // Calculate ActionBar height
         int  J = getResources().getDimensionPixelSize(R.dimen.abc_action_bar_default_height_material);
         int a = (height/4)+((int)nbb+(int)nbbb)-(J);
-        h=h-a;
-        this.player= new Image(context,(width/2)-(width/12),h,width/6,height/4, R.mipmap.perso);
 
+        yPlayerDraw=height-a;
 
-
-
-      //  player= new Player(this.getContext(),plateformList.get(0).getX(),plateformList.get(0).getY(),this.w/7,h/11,R.mipmap.p1_front);
-
-
+        this.playerDraw= new Image(context,(width/2)-(width/12),yPlayerDraw,width/6,height/4, R.mipmap.perso);
     }
 
     // Fonction qui "dessine" un écran de jeu
     public void doDraw(Canvas canvas) {
         if(canvas==null) {return;}
-
         // on efface l'écran, en blanc
         canvas.drawColor(Color.WHITE);
-        //player.onDraw(canvas);
-        this.player.draw(canvas);
-
-
-
+        this.playerDraw.draw(canvas);
       //  player.draw(canvas,plateformStay.getX(),plateformStay.getY() - player.getDrawH());
-
-
-
-
-
     }
 
     // Fonction appelée par la boucle principale (gameLoopThread)
@@ -164,74 +127,23 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     public boolean onTouchEvent(MotionEvent event) {
         final int x = (int) event.getRawX();//35
         final int y = (int) event.getRawY();
+
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
-          /*  case MotionEvent.ACTION_DOWN:
-                xCor = event.getX(); //35
-                ok=((xCor>=player.getX())&& (xCor<=player.getX()+player.getDrawW()));
 
-
-                break;
-            case MotionEvent.ACTION_MOVE:
-
-                if(ok) {
-
-                    if (event.getX() >= xCor) {//41
-                        this.xCorMove=event.getX()-xCor;
-                        player.setX(player.getX()+50);
-                        player.imgB.left
-                    }else {//39
-
-                        this.xCorMove=xCor-event.getX();
-                        player.setX(player.getX()-50);
-
-                    }
-                }
-
-
-                //view.animate().x(event.getRawX() + xCoOrdinate).y(event.getRawY() + yCoOrdinate).setDuration(0).start();
-                break;
-            default:
-                return false;*/
             case MotionEvent.ACTION_DOWN:
-                ok = ((x >= player.getX()) && (x <= player.getX() + player.getDrawW()));
-                xDelta = x - (int) player.getX(); //35-39=-4
+                ok = ((x >= playerDraw.getX()) && (x <= playerDraw.getX() + playerDraw.getDrawW()) && (y <= height ) && (y >= height - playerDraw.getDrawH()));
+                xDelta = x - (int) playerDraw.getX(); //35-39=-4
                 break;
 
             case MotionEvent.ACTION_MOVE:
                 if (ok){
-                    player.setX(x - xDelta);
+                    playerDraw.setX(x - xDelta);
                 }
-
                 break;
             default:
                 return false;
 
         }
-
-      /*  double currentX = event.getX();
-        if(currentX>=player.getX()){
-            player.setDirection(false);
-        }else {
-            player.setDirection(true);
-        }
-        double currentY = event.getY();
-        currentX = currentX-player.getX();
-        currentY=currentY-player.getY();
-        double angle = Math.atan2(currentX,currentY);
-
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                if(player.getState()==STATE.STAY || player.getState()==STATE.DOWN ) {
-                    player.calculeDirectionUp(w / 3);
-                    player.setAngle(angle);
-                    player.setState(STATE.UP);
-
-                }
-                break;
-        }*/
-
-
         return true;  // On retourne "true" pour indiquer qu'on a géré l'évènement
     }
 
@@ -240,35 +152,10 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     // nous obtenons ici la largeur/hauteur de l'écran en pixels
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int w, int h) {
-       /* balle.resize(w,h); // on définit la taille de la balle selon la taille de l'écran
-       bar2.resize(w,h);*/
-        this.h=h;
+        //
     }
 
     public void collision(){
-       /* double xPlayer= player.getX();
-        double yPlayer=player.getY();
-        double yPlateform;
-        for(Plateform p:plateformList){
-            yPlateform= (p.getY()-player.getDrawH());
-            double a = player.getY()+player.getDrawH();
-
-           // System.out.println("player x "+player.getX()+" plateform x "+plateformStay.getX() +" plat + w " + (plateformStay.getX()+plateformStay.getDrawW()));
-            if(
-                    (player.getX() >=  p.getX()) &&( player.getX() <= ( p.getX() + p.getDrawW())))
-
-            {
-
-                System.out.println("a "+a+" p +15  "+(p.getY()+15)+" p -15 y "+(p.getY()-15));
-                player.setState(STATE.DOWN);
-                this.plateformStay=p;
-                break;
-
-            }
-        }
-      //  System.out.println("player x "+player.getX()+" plateform x "+plateformStay.getX()+" x + w plateform "+(plateformStay.getX()+plateformStay.getDrawW()));
-*/
-
-
+        //
     }
 } // class GameView
